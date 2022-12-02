32daf99c2f7d63c5887eafd6b84a988fe35d1570
Hunter Monroe <huntis@outlook.com>
2022-12-02 16:21:49 -0700

    Feature/ hompage setup and styling

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,27 @@
 
+import { Link } from 'react-router-dom';
 import Nav from '../Nav/Nav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Nav /> 
+      {/* <Nav /> */}
+      <div className="image-container">
+        <img
+          className="homepage-logo"
+          src="/images/baggageLogo2.png"
+          alt="suitcase logo"
+        />
+      </div>
+      <div className="button-container">
+        <Link to="/">
+          <button className="homepage-button">Create Trip</button>
+        </Link>
+        <Link to="/">
+          <button className="homepage-button">Packing List</button>
+        </Link>
+      </div>
     </div>
   );
 }
