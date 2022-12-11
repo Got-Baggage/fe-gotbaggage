4cc7cbe89e63d101473f5fbb9e606d61ef32ff2e
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-12-11 12:36:36 -0700

    added stubbing to cypress test files

--- cypress/e2e/detailsPage.cy.js
+++ cypress/e2e/detailsPage.cy.js
@@ -1,5 +1,5 @@
-describe('empty spec', () => {
-  it('passes', () => {
+describe('Items page', () => {
+  it('Should be able to see all items for a trip', () => {
     cy.visit('https://example.cypress.io')
   })
 })
